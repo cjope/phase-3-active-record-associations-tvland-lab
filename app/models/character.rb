21a38636db8639ad34943e87133d882e5ca65357
Craig <craigjope@gmail.com>
2021-11-12 23:59:37 -0700

    complete

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,11 @@
 class Character < ActiveRecord::Base
+    belongs_to :show
+    # belongs_to :network, through: :shows
+    belongs_to :actor
+
+    def say_that_thing_you_say
+        "#{self.name} always says: #{self.catchphrase}"
+    end
   
+
 end
